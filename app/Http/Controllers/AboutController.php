06e46b26e96b955c5de544012103dcc595c0c642
Ali Zulfikar <62384197+alizul01@users.noreply.github.com>
2023-02-22 16:13:10 +0700

    Ans: <Number 2>(Controller)

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function index() {
+        echo "Nama : Muhammad Ali Zulfikar <br> NIM : 2141720088";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,14 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function() {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/about', function() {
-    echo "Nama : Muhammad Ali Zulfikar <br> NIM : 2141720088";
-});
+Route::get('/about', [AboutController::class, 'index']);
 
-Route::get('/articles/{id}', function($id) {
-    echo "Halaman Artikel dengan ID {$id}";
-});
+Route::get('/articles/{id}', [ArticleController::class, 'index']);
